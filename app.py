f8f126f57ef8eea2b989abc4a248501d88b8ef25
MarkEhlerNV <mark.ehler@northviewweather.com>
2024-02-06 14:29:25 -0500

    form route added

--- app.py
+++ app.py
@@ -23,5 +23,27 @@ def index():
         # If an exception occurs (e.g., connection error or file not found), serve a fallback landing page
         return send_from_directory('react-flask-deploy/build', '404.html')
 
+# pseudo-code mock-up findme
+@app.route('api/submit_form', methods=['POST'])
+def submit_form():
+    
+    if request.method == 'POST':
+        zipcode = request.form.get('zipcode')
+        email = request.form.get('email')
+        produce_type = request.form.getlist('produceType')
+
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO submissions (zipcode, email, produce_type)
+            VALUES (?, ?, ?)
+        ''', (zipcode, email, ', '.join(produce_type)))
+        conn.commit()
+        conn.close()
+
+        return jsonify({'status': 'success'})
+    else:
+        return jsonify({'status': 'error'})
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", threaded=True, port=5000)
